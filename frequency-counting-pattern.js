16eeb185bbf512a21f89a3d4d60511a819e8c3f5
Asadbek <81373435+asadbek2021@users.noreply.github.com>
2023-10-05 12:04:32 +0000

    Update frequency-counting-pattern.js

--- frequency-counting-pattern.js
+++ frequency-counting-pattern.js
@@ -26,4 +26,31 @@ function validAnagram(input1, input2) {
     return true;
 }
 
+function sameFrequency(num1, num2){
+    num1 = num1.toString();
+    num2 = num2.toString();
+
+    if(num1.length != num2.length) {
+        return false;
+    }
+
+    let lookup = {}
+    
+    for (let i = 0; i <= num1.length - 1; i++  ) {
+        let letter = num1[i];
+        lookup[letter] = 
+            lookup[letter] ? lookup[letter] + 1 : 1;
+    }
+    
+    for (let key of num2) {
+        if(lookup[key]) {
+            lookup[key]--;
+        } else {
+            return false
+        }
+    }
+    
+    return true;
+}
+
 // by time complexity O(n)
